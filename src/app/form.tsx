8cdcd9e33693358afdd5472fbdfdf57e3fc0c431
Paul <developerpaul1@gmail.com>
2025-01-26 12:55:05 +0100

    extremely tiny change

--- src/app/form.tsx
+++ src/app/form.tsx
@@ -45,6 +45,7 @@ export function Form({
   useEffect(() => {
     window.onTelegramAuth = (user: TelegramUser) => {
       setTelegramUser(user);
+      setCurrentStep(FormType.Year);
     };
 
     const script = document.createElement("script");
